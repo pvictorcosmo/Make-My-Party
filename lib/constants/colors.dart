cc73f56c7451222a68a4d58b8d98f687626b9531
RaissaDonato <112511214+RaissaDonato@users.noreply.github.com>
2023-04-17 23:02:45 -0300

    adding a new color

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 const Color appColorPurple = Color(0xFF6320EE);
 const Color appColorGrey = Color(0xFFD9D9D9);
+const Color appColorDarkGrey = Color(0x928B8B8B);
