43eea69d7db2dc9c517ee77b9e3fedf8435ab235
RaissaDonato <112511214+RaissaDonato@users.noreply.github.com>
2023-04-30 23:15:24 -0300

    Create register_name.dart

---
+++ lib/screens/register/register_name.dart
@@ -0,0 +1,173 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:make_my_party/constants/colors.dart';
+
+
+class RegisterNamePage extends StatefulWidget {
+  const RegisterNamePage({super.key});
+
+  @override
+  _RegisterNamePageState createState() => _RegisterNamePageState();
+}
+
+class _RegisterNamePageState extends State<RegisterNamePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: IconButton(
+                icon: Icon(Icons.keyboard_arrow_left),
+                color: appColorPurple,
+                onPressed: () {},
+                ),
+              ),
+            ],
+          ),
+        ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 15, vertical: 50),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              textAlign: TextAlign.left,
+              'Qual o seu nome e sobrenome?\n',
+              style: GoogleFonts.titilliumWeb(
+                fontSize: 16,
+                fontWeight: FontWeight.w800,
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(
+              height: 49,
+              width: 292,
+              child: Form(
+                child: TextFormField(
+                  maxLength: null,
+                  decoration: InputDecoration(
+                    labelText: "Nome",
+                    labelStyle: TextStyle(
+                      color: appColorGrey,
+                    ),
+                    floatingLabelBehavior: FloatingLabelBehavior.always,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: appColorGrey),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: appColorGrey)
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Text(
+              '\n'
+            ),
+            SizedBox(
+              height: 49,
+              width: 292,
+              child: Form(
+                child: TextFormField(
+                  maxLength: null,
+                  decoration: InputDecoration(
+                    labelText: "Sobrenome",
+                    labelStyle: TextStyle(
+                      color: appColorGrey,
+                    ),
+                    floatingLabelBehavior: FloatingLabelBehavior.always,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: appColorGrey),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: appColorGrey)
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Text(
+              textAlign: TextAlign.start,
+              '\nSeu nome completo ajuda o restaurante a saber quem\nestá pedindo',
+              style: GoogleFonts.poppins(
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                color: appColorDarkGrey,
+              ),
+            ),
+            RichText(
+              text: TextSpan(
+                text: '\nAo continuar, você concorda com os ',
+                style: GoogleFonts.poppins(
+                  color: Colors.black,
+                  fontWeight: FontWeight.normal,
+                  fontSize: 12,
+                ),
+                children: <TextSpan>[
+                  TextSpan(
+                    text: 'Termos de Uso\n',
+                    style: GoogleFonts.poppins(
+                      color: appColorPurple,
+                      fontSize: 12,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                  TextSpan(
+                    text: 'e a ',
+                    style: GoogleFonts.poppins(
+                      color: Colors.black,
+                      fontSize: 12,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                  TextSpan(
+                    text: 'Política de Privacidade',
+                    style: GoogleFonts.poppins(
+                      color: appColorPurple,
+                      fontSize: 12,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                ]
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: appColorPurple,
+        child: Container(
+          height: 82.0,
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text(
+                    'Criar conta',
+                    style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
